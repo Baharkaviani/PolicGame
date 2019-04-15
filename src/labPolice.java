a9aad47d042366991887dcda4f7f870839758af9
Bahar <bhr.kaviani@gmail.com>
2019-04-15 17:30:19 +0430

    PoliceBase

---
+++ src/labPolice.java
@@ -0,0 +1,2 @@
+public class labPolice {
+}
---
+++ src/police.java
@@ -0,0 +1,54 @@
+public class police {
+    private int x, y, lastX, lastY;
+    private boolean thiefSeen;
+
+    public police(int x, int y){
+        this.x = x;
+        this.y = y;
+        lastX = -1;
+        lastY = -1;
+        thiefSeen = false;
+    }
+
+    //getter
+    public int getLastX() {
+        return lastX;
+    }
+
+    public int getLastY() {
+        return lastY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isThiefSeen() {
+        return thiefSeen;
+    }
+
+    //setter
+    public void setLastX(int lastX) {
+        this.lastX = lastX;
+    }
+
+    public void setLastY(int lastY) {
+        this.lastY = lastY;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setThiefSeen(boolean thiefSeen) {
+        this.thiefSeen = thiefSeen;
+    }
+}
